a8f3cea00551f3e67d3602ae7e4d22e40b1fa85a
Douglas G. Moore <doug@dglmoore.com>
2017-12-17 18:23:55 +0000

    Update signature of inform_mutual_info

--- pyinform/mutualinfo.py
+++ pyinform/mutualinfo.py
@@ -43,7 +43,7 @@ from ctypes import byref, c_char_p, c_int, c_ulong, c_double, POINTER
 from pyinform import _inform
 from pyinform.error import ErrorCode, error_guard
 
-def mutual_info(xs, ys, bx=0, by=0, b=2.0, local=False):
+def mutual_info(xs, ys, bx=0, by=0, local=False):
     """
     Compute the (local) mutual information between two time series.
     
@@ -58,17 +58,17 @@ def mutual_info(xs, ys, bx=0, by=0, b=2.0, local=False):
     :type ys: a sequence or ``numpy.ndarray``
     :param int bx: the base of the first time series
     :param int by: the base of the second time series
-    :param double b: the logarithmic base
     :param bool local: compute the local mutual information
     :return: the local or average mutual information
     :rtype: float or ``numpy.ndarray``
     :raises ValueError: if the time series have different shapes
     :raises InformError: if an error occurs within the ``inform`` C call
     """
-    us = np.ascontiguousarray(xs, dtype=np.int32)
-    vs = np.ascontiguousarray(ys, dtype=np.int32)
+    us = np.asarray(xs, dtype=np.int32)
+    vs = np.asarray(ys, dtype=np.int32)
     if us.shape != vs.shape:
         raise ValueError("timeseries lengths do not match")
+    series = np.ascontiguousarray([us,vs])
 
     if bx == 0:
         bx = max(2, np.amax(us)+1)
@@ -76,27 +76,29 @@ def mutual_info(xs, ys, bx=0, by=0, b=2.0, local=False):
     if by == 0:
         by = max(2, np.amax(vs)+1)
 
-    xdata = us.ctypes.data_as(POINTER(c_int))
-    ydata = vs.ctypes.data_as(POINTER(c_int))
-    n = us.size
+    base = np.ascontiguousarray([bx,by], dtype=np.int32)
+    basedata = base.ctypes.data_as(POINTER(c_int))
+
+    seriesdata = series.ctypes.data_as(POINTER(c_int))
+    l, n = 2, us.size
 
     e = ErrorCode(0)
 
     if local is True:
         mi = np.empty(us.shape, dtype=np.float64)
         out = mi.ctypes.data_as(POINTER(c_double))
-        _local_mutual_info(xdata, ydata, c_ulong(n), c_int(bx), c_int(by), c_double(b), out, byref(e))
+        _local_mutual_info(seriesdata, c_ulong(l), c_ulong(n), basedata, out, byref(e))
     else:
-        mi = _mutual_info(xdata, ydata, c_ulong(n), c_int(bx), c_int(by), c_double(b), byref(e))
+        mi = _mutual_info(seriesdata, c_ulong(l), c_ulong(n), basedata, byref(e))
 
     error_guard(e)
 
     return mi
 
 _mutual_info = _inform.inform_mutual_info
-_mutual_info.argtypes = [POINTER(c_int), POINTER(c_int), c_ulong, c_int, c_int, c_double, POINTER(c_int)]
+_mutual_info.argtypes = [POINTER(c_int), c_ulong, c_ulong, POINTER(c_int), POINTER(c_int)]
 _mutual_info.restype = c_double
 
 _local_mutual_info = _inform.inform_local_mutual_info
-_local_mutual_info.argtypes = [POINTER(c_int), POINTER(c_int), c_ulong, c_int, c_int, c_double, POINTER(c_double), POINTER(c_int)]
-_local_mutual_info.restype = c_double
+_local_mutual_info.argtypes = [POINTER(c_int), c_ulong, c_ulong, POINTER(c_int), POINTER(c_double), POINTER(c_int)]
+_local_mutual_info.restype = POINTER(c_double)
